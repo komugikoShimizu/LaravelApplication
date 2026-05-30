94d724947f1f9dab999da3532bb5bf731d077d46
shimizusumito <nkcshimizusumito@gmail.com>
2026-05-30 23:23:17 +0900

    APIを定義

--- src/app/Http/Controllers/CategolyController.php
+++ src/app/Http/Controllers/CategolyController.php
@@ -1,5 +1,16 @@
 <?php
 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
 class CategolyController
 {
+    public function __construct
+    (){}
+
+    public function getCategolies(Request $req)
+    {
+
+    }
 }
